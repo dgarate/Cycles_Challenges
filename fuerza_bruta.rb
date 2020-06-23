d9fd9f7f68e61df970eee600136e15069436dd3b
dgarate <garatex@gmail.com>
2020-06-22 23:26:17 -0400

    commit for Cycle Challenges

---
+++ fuerza_bruta.rb
@@ -0,0 +1,12 @@
+
+password = ARGV[0]
+
+intento = "a"
+cantidad = 0
+
+while intento != password
+   intento = intento.next
+   cantidad += 1
+end
+
+print cantidad 
---
+++ solo_pares.rb
@@ -0,0 +1,2 @@
+user_number = ARGV[0].to_i
+user_number.times {|i| print i*2 }
